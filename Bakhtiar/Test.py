a629a4fd2e5aca85fa102b5bc36084bfa169390f
Bakhtiar Hasan <bakhtiarhasan@iut-dhaka.edu>
2020-09-19 14:21:05 +0600

    Testing DTW

--- Bakhtiar/Test.py
+++ Bakhtiar/Test.py
@@ -2,6 +2,7 @@ import os
 import Preprocess
 import LoadData
 import TopNPairIndex
+import DTW
 
 def test(fileName, datasetConst, genConst):
     currentFolderName = os.getcwd()
@@ -9,9 +10,31 @@ def test(fileName, datasetConst, genConst):
     preprocessedTestData, frameCount = Preprocess.preprocess(fileNameWithPath, datasetConst, genConst)
     trainData, metaData = LoadData.loadData(genConst)
 
-    N = metaData["N"]
     topNPairIndex = metaData["TopNPairIndex"]
     topNTestPairFrames = TopNPairIndex.extractTopNIndex(preprocessedTestData, topNPairIndex)
     
-    
+    userCount = len(trainData)
+    voteCount = [0] * (userCount + 1)
+    storeMismatch = [0] * (userCount + 1)
+    print(DTW.dtw([1, 2, 3, 4], [1, 2, 3, 4]))
+"""
+    for user in range(1, userCount + 1):
+        storedTrainFrameCount = trainData[str(user)][0]
+        storedTrainData = trainData[str(user)][1]
+        testData = topNTestPairFrames
+        testFrameCount = frameCount
+        totalMismatch = 0
+        for idx in range(metaData["N"]):
+            trainDTW = []
+            testDTW = []
+            for frame in range(storedTrainFrameCount):
+                trainDTW.append(storedTrainData[frame][idx])
+            for frame in range(testFrameCount):
+                testDTW.append(testData[frame][idx])
+            totalMismatch += DTW.dtw(trainDTW, testDTW)
+        storeMismatch[user] = totalMismatch
+    print(storeMismatch)
+
+"""
+
     
