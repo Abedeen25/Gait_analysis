bb92b3c28db40ab2b82814023f1c7e25c576022c
Bakhtiar Hasan <bakhtiarhasan@iut-dhaka.edu>
2020-09-19 13:36:53 +0600

    training phase complete

--- Bakhtiar/StoreData.py
+++ Bakhtiar/StoreData.py
@@ -1,4 +1,39 @@
-def storeData(data, topNPairIndex, genConst, datasetConst):
+import os
+import json
+from math import comb
+
+def storeData(data, topNPairIndex, genConst):
     userCount = len(data)
     dataForStoring = {}
-    for 
+    pairCount = comb(genConst['TotalJoints'] - 1, 2)
+    maxFrames = -1;
+    for user in range(1, userCount + 1):
+        frameCount = data[str(user)][0]
+        maxFrames = max(frameCount, maxFrames)
+        dataArray = data[str(user)][1]
+        topNPairFrames = []
+        for frame in dataArray:
+            topNPairs = []
+            for idx in topNPairIndex:
+                topNPairs.append(frame[idx])
+            topNPairFrames.append(topNPairs)
+        dataForStoring[str(user)] = []
+        dataForStoring[str(user)].append(frameCount)
+        dataForStoring[str(user)].append(topNPairFrames)
+    currentFolderName = os.getcwd()
+    trainingSetFileName = "train.json"
+    trainingFileWithPath = os.path.join(currentFolderName, genConst['TrainingSetFolder'], trainingSetFileName)
+    with open(trainingFileWithPath, 'w') as outFile:
+        json.dump(dataForStoring, outFile)
+
+    meta = {}
+    meta["UserCount"] = userCount
+    meta["MaxFrames"] = maxFrames
+    meta["N"] = len(topNPairIndex)
+    meta["TopNPairIndex"] = topNPairIndex
+    metadataFileName = "metadata.json"
+    metadataFileWithPath = os.path.join(currentFolderName, genConst['TrainingSetFolder'], metadataFileName)
+    with open(metadataFileWithPath, 'w') as outFile:
+        json.dump(meta, outFile)
+        
+
--- Bakhtiar/train.py
+++ Bakhtiar/train.py
@@ -3,21 +3,20 @@ import math
 import json
 import Preprocess
 import TopNPairIndex
+import StoreData
 
 def train(userCount, datasetConst, genConst):
-    maxFrameCount = 0
     dataFromFile = {}
     for userID in range(1, userCount + 1):
         currentFolderName = os.getcwd()
         currentFileName = "u" + str(userID) + "s1.txt"
         fileNameWithPath = os.path.join(currentFolderName, genConst['DatasetFolder'], datasetConst['Folder'], currentFileName)
         preprocessedTrainData, frameCount = Preprocess.preprocess(fileNameWithPath, datasetConst, genConst)
-        maxFrameCount = max(frameCount, maxFrameCount)
         dataFromFile[str(userID)] = []
         dataFromFile[str(userID)].append(frameCount)
         dataFromFile[str(userID)].append(preprocessedTrainData)
     topNPairIndex = TopNPairIndex.topNPairIndex(dataFromFile, genConst)
-
+    StoreData.storeData(dataFromFile, topNPairIndex, genConst)
 
         
     
